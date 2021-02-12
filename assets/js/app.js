65a79e2859edecd207578971bdcb5b522ff288e9
Leo <leodavin4y@yandex.ru>
2021-02-12 21:35:06 +0500

    Откатил, не работает

--- assets/js/app.js
+++ assets/js/app.js
@@ -27,19 +27,6 @@ import shopStore from './stores/shopStore';
 // для IE11
 require("es6-object-assign").polyfill();
 
-function preventDefault(e) {
-    e.preventDefault();
-}
-
-function disableScroll() {
-    document.body.addEventListener('touchmove', preventDefault, { passive: false });
-}
-function enableScroll() {
-    document.body.removeEventListener('touchmove', preventDefault);
-}
-
-disableScroll();
-
 const GetCoinsPage = React.lazy(() => import('../js/getcoins/GetCoinsPage'));
 const StatsPage = React.lazy(() => import('../js/stats/StatsPage'));
 const DashboardPage = React.lazy(() => import('../js/admin/dashboard/DashboardPage'));
