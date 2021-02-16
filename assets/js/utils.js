d6851fa046a2a2c929faf2dab8b740263483ff33
Leo <leodavin4y@yandex.ru>
2021-02-16 17:36:22 +0500

    Вынес вкладку инфо в компонент. Отменяемые промисы. Конвертаторы из бд

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -119,3 +119,23 @@ export async function storageSet(key, value)
             return false;
         });
 }
+
+export function makeCancelable(promise) {
+    let hasCanceled_ = false;
+
+    const wrappedPromise = new Promise((resolve, reject) => {
+        promise.then((val) =>
+            hasCanceled_ ? reject({isCanceled: true}) : resolve(val)
+        );
+        promise.catch((error) =>
+            hasCanceled_ ? reject({isCanceled: true}) : reject(error)
+        );
+    });
+
+    return {
+        promise: wrappedPromise,
+        cancel() {
+            hasCanceled_ = true;
+        },
+    }
+}
